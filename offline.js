7cd51c7e2b825645aefa3ac5ee4a06b12e065759
setanan <setanan135@gmail.com>
2019-06-20 14:22:09 +0700

    runner
    runner

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1561014634,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pleyer-sheet0.png",
+		"images/pleyer-sheet1.png",
+		"images/pleyer-sheet2.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
